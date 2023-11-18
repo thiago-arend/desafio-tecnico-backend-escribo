9e2c2433a5aab7d213d5e93ab550463a42fb8f5e
thiago-arend <thiago.arend@gmail.com>
2023-11-17 22:17:55 -0300

    feat: sumAllIntegersDivisible function finished

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function sumAllIntegersDivisible(limit) {
+    let accumulatedSum = 0;
+
+    for (let i = 3; i < limit; i++) {
+        if ( (i % 3 === 0 ) || ( i % 5 === 0 )) {
+            accumulatedSum += i;
+        }
+    }
+
+    return accumulatedSum;
+}
+
+const parameterFromTerminal = Number(process.argv[2]);
+console.log(sumAllIntegersDivisible(parameterFromTerminal));
